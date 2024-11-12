77dad6067d3434abbf8d1c36d8d71a4130541c2b
João Pedro <leon.jpbs@gmail.com>
2024-11-12 11:24:27 -0300

    Feito layout

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,17 +1,12 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/script.js"></script>
-</head>
-<body>
-    
+@extends('layouts.main')
+
+@section('title', 'HDC Events')
+
+@section('content')
+
+
 
 <h1>a</h1>
 
 
-</body>
-</html>
+@endsection
