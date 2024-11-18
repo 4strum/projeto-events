78b1b1daba87dc3bbc974d7b19e06226e7bbba3d
João Pedro <leon.jpbs@gmail.com>
2024-11-18 13:18:32 -0300

    feito cadastro de evento

--- routes/web.php
+++ routes/web.php
@@ -5,9 +5,9 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EventController;
 
 Route::get('/', [EventController::class, 'index']);
+Route::get('/events/create', [EventController::class, 'create']);
+Route::POST('/events', [EventController::class, 'store']);
 
 
 
-Route::get('/produtos_teste/{id}', function ($id = null) {
-    return view('product', ['id' => $id]);
-});
+
