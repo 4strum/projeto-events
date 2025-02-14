058283103ca6145933af422d61ee4e4026ed558c
João Pedro <leon.jpbs@gmail.com>
2025-02-14 11:05:47 -0300

    Exibindo dados do usuario na view

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Event;
+use App\Models\User;
 
 class EventController extends Controller
 {
@@ -72,6 +73,8 @@ class EventController extends Controller
     {
         $event = Event::find($id);
 
-        return view('events.show', ['event' => $event]);
+        $eventOwner =  User::where('id', $event->user_id)->first()->toArray();
+
+        return view('events.show', ['event' => $event, 'eventOwner' => $eventOwner]);
     }
 }
